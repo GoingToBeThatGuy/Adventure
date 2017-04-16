1b157976f5b5b5304d54814fcfb4ac4b24995800
Marc Gleaosn <anthonyg1027@yahoo.com>
2017-04-16 17:23:05 -0500

    Update Basic_Functions.h

--- Basic_Functions.h
+++ Basic_Functions.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 class Basic_Functions {
 
 public:
@@ -8,3 +9,24 @@ public:
 	template <class Temp>
 	Temp Confirm(std::string Prompt, Temp given);
 };
+
+template<class Temp>
+inline Temp Basic_Functions::Confirm(std::string Prompt, Temp given)
+{
+	char choice;
+	std::cout << "Confirm: " << given << "? (Y/N)" << std::endl;
+	cin >> choice;
+	switch (choice) {
+	case 'Y':
+		return given;
+		break;
+	case 'N':
+		std::cout << Prompt << std::endl;
+		return Confirm(Prompt, given);
+		break;
+	default:
+		std::cout << "Incorrect answer, try again..." << std::endl << std::endl;
+		std::cout << Prompt << std::endl;
+		return Confirm(Prompt, given);
+	}
+	return Temp();
